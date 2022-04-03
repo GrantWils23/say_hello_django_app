f8ebea776498775a4c694bbd9f3dd6c0f7d2c8bd
Grant <GrantWils.23@googlemail.com>
2022-04-03 13:33:08 +0000

    add coverage using pip and create full test coverage via course material picking up all less covered material

--- todo/test_views.py
+++ todo/test_views.py
@@ -24,17 +24,24 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
 
     def test_can_delete_item_function(self):
-        item = Item.objects.create(name='test_todo_item')
+        item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
         existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
 
     def test_can_toggle_item_button(self):
-        item = Item.objects.create(name='test_todo_item', done=True)
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
 
+    def test_can_edit_item(self):
+        item = Item.objects.create(name="Test Todo Item")
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
 
+        
